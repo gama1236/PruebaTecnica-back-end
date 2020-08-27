fad85430e44901508d581d0a5994a73ae3892570
Stiven Castro <stiven.castroq@premize.com>
2020-08-26 19:16:52 -0500

    initial proyect

---
+++ src/test/java/com/pruebatecnica/codesa/CodesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.codesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
